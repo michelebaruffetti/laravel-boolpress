14e3803f1dd0ec7170aefd8b3ff406a69ac9b4ce
michele baruffetti <michelebaruffetti@gmail.com>
2020-07-14 15:40:49 +0200

    Primo Commit - ieri mi ero scordato (e sono in recupero della lezione)

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.dashboard')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-10">
+            <h1>DASHBOARD ADMIN</h1>
+
+        </div>
+
+    </div>
+
+</div>
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-1O">
+            <h1>LANDING PAGE</h1>
+        </div>
+    </div>
+</div>
+@endsection
