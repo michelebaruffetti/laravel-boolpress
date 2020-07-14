9cc43fd8ea80498c2d7c2ac7e12330c149a3f47e
michele baruffetti <michelebaruffetti@gmail.com>
2020-07-14 16:07:05 +0200

    Lavorando sulle CRUD per i post del blog.

---
+++ app/Http/Controllers/Admin/PostController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Post;
+
+class PostController extends Controller
+{
+
+    public function index()
+    {
+        $posts = Post->all();
+        return view('admin..post.index' compact('posts'));
+    }
+
+
+    public function create()
+    {
+        //
+    }
+
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+
+    public function show($id)
+    {
+        //
+    }
+
+    public function edit($id)
+    {
+        //
+    }
+
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+
+    public function destroy($id)
+    {
+        //
+    }
+}
