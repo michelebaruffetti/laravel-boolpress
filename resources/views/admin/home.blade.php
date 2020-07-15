abfc31107ce68070ef5f37edfe7514dd59888f85
michele baruffetti <michelebaruffetti@gmail.com>
2020-07-15 10:00:07 +0200

    Aggiunta bottone per vedere post su index posts

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-10">
             <h1>DASHBOARD ADMIN</h1>
-            
+            <a class="btn btn-primary"href="{{ route('admin.posts.index') }}">Showami i POSTS!</a>
         </div>
 
     </div>
